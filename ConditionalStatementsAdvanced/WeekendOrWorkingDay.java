2dd316115af0671e5a14d80923bb48d3e5b5ccb1
RosiAvramova <106426969+RosiAvramova@users.noreply.github.com>
2022-09-13 13:43:43 +0300

    Conditional Statements - Advanced

---
+++ ConditionalStatementsAdvanced/WeekendOrWorkingDay.java
@@ -0,0 +1,25 @@
+package ConditionalStatementsAdvanced;
+
+import java.util.Scanner;
+
+public class WeekendOrWorkingDay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nameOfTheDay = scanner.nextLine();
+        switch (nameOfTheDay){
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                System.out.println("Working day");
+                break;
+            case "Saturday":
+            case "Sunday":
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Error");
+        }
+    }
+}
